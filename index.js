95b3c9289676b56e07984b4d827c03061af5b0e5
unknown <jasmineakbari@yahoo.com>
2020-12-20 16:45:03 -0500

    completed questions to be prompted

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is your project name? (Required)',
-        validate: nameInput =>{
+        message: 'Please enter your project name? (Required)',
+        validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
@@ -19,8 +20,21 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'github',
+        message: 'Please enter your github username. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
         name: 'description',
-        message: 'Please enter a description for your project (Required)',
+        message: 'Please enter a description for your project. (Required)',
         validate: nameInput => {
             if (nameInput) {
                 return true;
@@ -30,11 +44,42 @@ const questions = [
             }
         }
     },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Would you like to provide installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Would you like to add helpful images and more instrcutions for use?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Would you like to add information for people who would like to contribute to your project?'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Do you have any licenses to list? (Check all that apply)',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD']
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Do you have any conducted test results to share?'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'would you like to add your email to receive questions?'
+    }
 ];
 
 // TODO: Create a function to write README file
 const writeFile = (fileName, data => {
-
+    
 })
 
 // function to initialize app

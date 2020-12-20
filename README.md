c943b64925fc875004004c00ea1900ac6ada73c2
unknown <jasmineakbari@yahoo.com>
2020-12-20 16:26:24 -0500

    altered readme and started prompt questions function

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Professional README Generator
+# README Generator
 
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+
+## Usage 
+
+Helpful Screenshots:
+![alt text](./images/)
+
+
+
+## Credits
+
+initial code provided at: https://github.com/coding-boot-camp/potential-enigma.
 [How to create a Professional README](./readme-guide.md)
+
+
+## Contributing
+
+Made with ❤️ by: https://github.com/jasmineakbari
+
+If you would like to contribute to this work please feel free to submit any push/pull request, create issues or reach out to me at my email: jasmineakbari@yahoo.com.
--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your project name? (Required)',
+        validate: nameInput =>{
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description for your project!')
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = (fileName, data => {
 
-// TODO: Create a function to initialize app
-function init() {}
+})
+
+// function to initialize app
+const init = (questions)
 
 // Function call to initialize app
 init();
